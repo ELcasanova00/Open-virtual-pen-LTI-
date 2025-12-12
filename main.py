72346da5998ee0edcc59040d3db1a586315ad5fe
ELcasanova00 <channelsyam98@gmail.com>
2025-12-12 17:55:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
 
-# --- Warna yang mau dideteksi (contoh: biru) ---
+
 lower_color = np.array([100, 150, 50])
 upper_color = np.array([140, 255, 255])
 
-# Webcam
+
 cap = cv2.VideoCapture(0)
 
-# Canvas untuk menggambar
+
 canvas = None
 
-# Posisi sebelumnya
+
 x_prev, y_prev = None, None
 
 while True:
@@ -21,19 +21,19 @@ while True:
 
     frame = cv2.flip(frame, 1)
 
-    # Buat canvas pertama kali saat size diketahui
+   
     if canvas is None:
         canvas = np.zeros_like(frame)
 
-    # Convert ke HSV
+    
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Mask warna
+   
     mask = cv2.inRange(hsv, lower_color, upper_color)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
-    # Cari contour warna
+    
     cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if len(cnts) > 0:
@@ -53,7 +53,7 @@ while True:
         # Reset jika warna hilang
         x_prev, y_prev = None, None
 
-    # Gabungkan frame asli + canvas
+   
     output = cv2.add(frame, canvas)
 
     cv2.imshow("Virtual Pen", output)
@@ -67,3 +67,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
